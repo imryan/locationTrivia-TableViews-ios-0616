9200adbb6d9d6a92b085b730173706c184865b95
imryan <notryancohen@gmail.com>
2016-06-23 13:04:30 -0400

    extra credit remake

--- locationTrivia-tableviews/FISLocationsTableViewController.m
+++ locationTrivia-tableviews/FISLocationsTableViewController.m
@@ -18,8 +18,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.title = @"Location Trivia";
+    
     self.tableView.accessibilityLabel = @"Locations Table";
     self.tableView.accessibilityIdentifier = @"Locations Table";
+    
+    FISLocation *location1 = [[FISLocation alloc] initWithName:@"The Empire State Building"
+                                                      latitude:40.7484
+                                                     longitude:-73.9857];
+    
+    FISTrivium *trivia1A = [[FISTrivium alloc] initWithContent:@"1,454 Feet Tall" likes:4];
+    FISTrivium *trivia1B = [[FISTrivium alloc] initWithContent:@"Cost $24,718,000 to build" likes:2];
+    
+    [location1.trivia addObjectsFromArray:@[trivia1A, trivia1B]];
+    
+    FISLocation *location2 = [[FISLocation alloc] initWithName:@"Bowling Green"
+                                                      latitude:41.3739
+                                                     longitude:-83.6508];
+    
+    FISTrivium *trivia2A = [[FISTrivium alloc] initWithContent:@"NYC's oldest park" likes:8];
+    FISTrivium *trivia2B = [[FISTrivium alloc] initWithContent:@"Made a park in 1733" likes:2];
+    FISTrivium *trivia2C = [[FISTrivium alloc] initWithContent:@"Charging Bull was created in 1989" likes:0];
+    
+    [location2.trivia addObjectsFromArray:@[trivia2A, trivia2B, trivia2C]];
+    
+    FISLocation *location3 = [[FISLocation alloc] initWithName:@"Statue Of Liberty"
+                                                      latitude:40.6892
+                                                     longitude:74.0444];
+    FISTrivium *trivia3A = [[FISTrivium alloc] initWithContent:@"Gift from the french" likes:6];
+    
+    [location3.trivia addObjectsFromArray:@[trivia3A]];
+    
+    self.locations = @[location1, location2, location3];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -30,36 +60,37 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return self.locations.count;
 }
 
-//- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-//    return [self.locations[section] name];
-//}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.locations[section] trivia].count;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    return [self.locations[section] name];
+}
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CellId" forIndexPath:indexPath];
     
-    FISLocation *location = (FISLocation *)self.locations[indexPath.row];
+    cell.accessoryType = UITableViewCellAccessoryNone;
     
-    cell.textLabel.text = location.name;
-    cell.detailTextLabel.text = [NSString stringWithFormat:@"%lu", location.trivia.count];
+    FISLocation *location = (FISLocation *)self.locations[indexPath.section];
+    FISTrivium *trivium = location.trivia[indexPath.row];
+    
+    cell.textLabel.text = trivium.content;
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%lu likes", trivium.likes];
     
     return cell;
 }
 
-#pragma mark - Navigation
-
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    FISTriviaTableViewController *detail = (FISTriviaTableViewController *)[segue destinationViewController];
-    FISLocation *location = self.locations[[self.tableView indexPathForSelectedRow].row];
-    
-    detail.trivia = [NSArray arrayWithArray:location.trivia];
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
+- (BOOL)shouldPerformSegueWithIdentifier:(NSString *)identifier sender:(id)sender {
+    return NO;
+}
 
 @end
